664049321d919a2afbf4bf54a925e51c89889387
sams00m <86266451+sams00m@users.noreply.github.com>
2023-12-04 18:19:23 -0500

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var test = require('unit.js');
-var index = require('../index.js');
+var index = require('../App.jsx');
 
 describe('Tests index', function() {
   it('verifies successful response', function(done) {
